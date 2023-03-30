54dc059e1d5db158a0b744664cbf0dfc4fe23da5
Andrei C <andreyeurope95hd@gmail.com>
2023-03-30 09:47:42 +0200

    Use unsplash api for searching

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -7,7 +7,7 @@ type ResponseData = {
 
 const searchApiKey = process.env.IMAGE_SEARCH_API_KEY || "";
 const searchApiBaseUrl = process.env.IMAGE_SEARCH_API_URL || "";
-const searchRoute = "/search";
+const searchRoute = "/search/photos";
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,12 +16,13 @@ export default async function handler(
   const searchTerm = (req.query.query as string) || "";
   const searchApiUrl = new URL(searchApiBaseUrl + searchRoute);
   searchApiUrl.searchParams.append("query", searchTerm);
+  searchApiUrl.searchParams.append("page", "1");
 
   const response = await fetch(searchApiUrl.href, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: searchApiKey,
+      Authorization: `Client-ID ${searchApiKey}`,
     },
   });
 
