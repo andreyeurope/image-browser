11454f9e08a390032c7cea9642c3198656b5f910
Andrei C <andreyeurope95hd@gmail.com>
2023-03-30 09:47:54 +0200

    Update types

--- src/types/search.ts
+++ src/types/search.ts
@@ -1,32 +1,62 @@
 export interface SearchResult {
-  total_results: number;
-  page: number;
-  per_page: number;
-  photos: Photo[];
-  next_page: string;
+  total: number;
+  total_pages: number;
+  results: Photo[];
 }
 
 export interface Photo {
-  id: number;
+  id: string;
+  created_at: Date;
   width: number;
   height: number;
-  url: string;
-  photographer: string;
-  photographer_url: string;
-  photographer_id: number;
-  avg_color: string;
-  src: ImageSource;
-  liked: boolean;
-  alt: string;
+  color: string;
+  blur_hash: string;
+  likes: number;
+  liked_by_user: boolean;
+  description?: string;
+  alt_description?: string;
+  user: User;
+  current_user_collections: any[];
+  urls: Urls;
+  links: ResultLinks;
 }
 
-export interface ImageSource {
-  original: string;
-  large2x: string;
-  large: string;
-  medium: string;
+export interface ResultLinks {
+  self: string;
+  html: string;
+  download: string;
+}
+
+export interface Urls {
+  raw: string;
+  full: string;
+  regular: string;
   small: string;
-  portrait: string;
-  landscape: string;
-  tiny: string;
+  thumb: string;
+}
+
+export interface User {
+  id: string;
+  username: string;
+  name: string;
+  first_name: string;
+  last_name: string;
+  instagram_username: string;
+  twitter_username: string;
+  portfolio_url: string;
+  profile_image: ProfileImage;
+  links: UserLinks;
+}
+
+export interface UserLinks {
+  self: string;
+  html: string;
+  photos: string;
+  likes: string;
+}
+
+export interface ProfileImage {
+  small: string;
+  medium: string;
+  large: string;
 }
